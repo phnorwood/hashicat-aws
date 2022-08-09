f98ca7fe959a495e78dda21930c999c339cbd11e
Patrick H. Norwood <patrick.h.norwood@gmail.com>
2022-08-09 03:09:43 +0000

    Added s3 bucket module

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -3,9 +3,7 @@ module "s3_bucket" {
   source  = "app.terraform.io/phnorwood-hc-training/s3-bucket/aws"
   version = "2.8.0"
 
-  bucket        = "my-s3-bucket"
   acl           = "private"
-  bucket_prefix = "phnorwood"
 
   versioning = {
     enabled = true
